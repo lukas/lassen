b95f15e8e4854a93416dd9283e190bb4281c36bb
Lukas Biewald <lukeab@gmail.com>
2017-04-05 10:12:06 -0700

    Commented test suite

--- tests.py
+++ tests.py
@@ -6,6 +6,12 @@ import numpy as np
 class TestNetwork(unittest.TestCase):
 
     def test_single_conv_layer(self):
+        """Tests a convolutional layer
+        (4x4) input = [[0,1,2,3], ... [12,13,14,15]]
+        biases = 1
+        (3x3) weights = [[0,1,2], ... [6,7,8]]
+        sum of output should be 3406"""
+
         img=np.reshape(np.arange(0,16),(4,4))
         network= [nets.ConvLayer((4,4), (3, 3), 1, 1)]
         biases = np.array([1.])
@@ -19,6 +25,12 @@ class TestNetwork(unittest.TestCase):
         self.assertEqual(my_sum, true_sum)
 
     def test_single_max_pool_layer(self):
+        """Tests a max pool layer
+        (4x4x2) input = [[[0,1,2,3], ... [12,13,14,15]], 
+                         [[16,17,18,19] ... [28, 29, 30, 31]]] 
+        (2x2) Max pool
+        sum of output should be 164"""
+
         img=np.reshape(np.arange(0,32),(4,4,2))
         img=np.transpose(img, (2,0,1))
         network=[nets.MaxPoolLayer((4,4), (2,2), 2)]
@@ -29,6 +41,12 @@ class TestNetwork(unittest.TestCase):
         self.assertEqual(my_sum, true_sum)
 
     def test_single_dense_layer(self):
+        """Tests a dense layer
+        (4) input = [0,1,2,3]
+        (4x3) weights = [[0,1,2] ... [9,10,11]]
+        (3) biases = [0,1,2]
+        sum of output should be 147"""
+
         # input size 4, output size 3
         img=np.arange(4)
         weights = np.reshape(np.arange(12),(4,3))
